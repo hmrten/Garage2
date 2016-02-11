6a3127d4fcd4a6be2a287f35437bc7247600f629
Mårten Hansson <hmrten@gmail.com>
2016-02-11 11:27:00 +0100

    add new vehicle contoller and migration Seed() for some random test data.

---
+++ Garage2/Controllers/VehicleController.cs
@@ -0,0 +1,20 @@
+﻿using Garage2.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Garage2.Controllers
+{
+    public class VehicleController : Controller
+    {
+        private VehicleContext db = new VehicleContext();
+        
+        // GET: Vehicle
+        public ActionResult Index()
+        {
+            return View(db.Vehicles);
+        }
+    }
+}
---
+++ Garage2/Migrations/Configuration.cs
@@ -0,0 +1,64 @@
+namespace Garage2.Migrations
+{
+    using Garage2.Models;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Garage2.DataAccess.VehicleContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        private string RandomRegNr(Random rnd)
+        {
+            var s = new char[6];
+            for (int i = 0; i < 3; ++i)
+                s[i] = (char)('A' + (rnd.Next() % 26));
+            for (int i=3; i<s.Length; ++i)
+                s[i] = (char)('0' + (rnd.Next() % 10));
+            return new string(s);
+        }
+
+        private string RandomName(Random rnd)
+        {
+            string[] fnames = { "John", "Joe", "Bob", "Alice", "Eve", "Chris" };
+            string[] lnames = { "Smith", "Forester", "Alison", "Carter", "Dole" };
+            return fnames[rnd.Next(0, fnames.Length)] + lnames[rnd.Next(0, lnames.Length)];
+        }
+
+        private DateTime RandomDate(Random rnd)
+        {
+            return new DateTime(DateTime.Now.Ticks + rnd.Next(100000, 200000000));
+        }
+
+        private VehicleType RandomType(Random rnd)
+        {
+            return (VehicleType)rnd.Next(0, (int)VehicleType.Last);
+        }
+
+        private Vehicle RandomVehicle(Random rnd)
+        {
+            var v = new Vehicle();
+            v.RegNr = RandomRegNr(rnd);
+            v.Owner = RandomName(rnd);
+            v.DateIn = RandomDate(rnd);
+            v.DateOut = RandomDate(rnd);
+            v.Type = RandomType(rnd);
+            return v;
+        }
+
+        protected override void Seed(Garage2.DataAccess.VehicleContext context)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < 100; ++i)
+            {
+                context.Vehicles.AddOrUpdate(v => v.RegNr, RandomVehicle(rnd));
+            }
+            context.SaveChanges();
+        }
+    }
+}
