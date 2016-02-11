c6a6a6432a4dde60f0dec6735ca131d7328de398
Mårten Hansson <hmrten@gmail.com>
2016-02-11 15:57:41 +0100

    redid migrations

--- Garage2/Migrations/201602111457134_Initial.Designer.cs
+++ Garage2/Migrations/201602111457134_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Garage2.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.1-30610")]
-    public sealed partial class initial : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201602111428042_initial"; }
+            get { return "201602111457134_Initial"; }
         }
         
         string IMigrationMetadata.Source
