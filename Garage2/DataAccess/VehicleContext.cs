21fb6787777de130370c0fbbe4df7b8e77cc1cda
Mårten Hansson <hmrten@gmail.com>
2016-02-11 11:19:52 +0100

    created model and an empty controller with a basic seed method for randomd data.

---
+++ Garage2/DataAccess/VehicleContext.cs
@@ -0,0 +1,15 @@
+﻿using Garage2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.DataAccess
+{
+    public class VehicleContext : DbContext
+    {
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public VehicleContext() : base("DefaultConnection") { }
+    }
+}
---
+++ Garage2/Models/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Models
+{
+    public enum VehicleType
+    {
+        Car,
+        Motorcycle,
+        Bus,
+        Truck,
+        
+        Last
+    }
+
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+        public VehicleType Type { get; set; }
+        public string RegNr { get; set; }
+        public string Owner { get; set; }
+        public DateTime DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+    }
+}
