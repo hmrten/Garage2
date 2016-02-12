c95dbd26965c21d52697cbc9daeb273f4b9e5a83
nvorkapic <nikola.vorkapic@hotmail.com>
2016-02-12 15:43:32 +0100

    Horrible Overview

---
+++ Garage2/ViewModels/Overview.cs
@@ -0,0 +1,34 @@
+﻿using Garage2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.ViewModels
+{
+	public class Overview
+	{
+
+		[Required, StringLength(50), Display(Name = "Registration number")]
+		public string VehicleReg { get; set; }
+
+		[Required]
+		public int ParkingSlotId { get; set; }
+
+		[Display(Name = "Date in")]
+		public DateTime DateIn { get; set; }
+
+		[Display(Name = "Date out")]
+		public DateTime DateOut { get; set; }
+
+		
+		[Required, Display(Name ="Vehicle Type")]
+		public VehicleType Type { get; set; }
+
+		[Required, StringLength(100)]
+		public string Owner { get; set; }
+
+		public TimeSpan Duration { set; get; }
+	}
+}
